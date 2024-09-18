4beef746ec045dd5d40957727ded14ebbb7d4468
LuizPauloJRM <luizpaulomedeiros77@hotmail.com>
2024-09-18 00:26:19 -0300

    Aula 04 : Java Poo Getters e setters

---
+++ Aulas/Curso_Poo/Aulas/Aula04/src/main/java/aula04/aula04/Aula04.java
@@ -0,0 +1,14 @@
+
+package aula04.aula04;
+
+
+public class Aula04 {
+
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta("Faber","Amarela", 0.4f);
+        c1.status();
+        Caneta c2 = new Caneta("Pentel","Preta",0.7f); //Nova baseado na classe 
+        c2.status();
+        
+    }
+}
